55ccca4fae6e5c7e023a7ef2fe6ecb4446f36649
qmzrjf <qmzrjf12@gmail.com>
2020-04-16 11:09:12 +0300

    --all

--- src/currency_exchange/settings_test.py
+++ src/currency_exchange/settings_test.py
@@ -5,13 +5,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'qmzrjf123@gmail.com'
-EMAIL_HOST_PASSWORD = 'BS8xYbtUW3'
 
 CELERY_ALWAYS_EAGER = CELERY_TASK_ALWAYS_EAGER = True
 
